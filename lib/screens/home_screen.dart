58ec1b9727c65a6e4e9ce7bca7a486201a368d1c
Abdiwahid <abdiwahidguhad@gmail.com>
2023-09-15 00:41:24 +0300

    bottom nav

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,13 +9,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int selectIndex = 0;
+  List<Widget> _widgetList = [];
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-        child: Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
-      ),
-    ));
+    return SafeArea(
+      child: Scaffold(
+          bottomNavigationBar: ConvexAppBar(
+        items: const [
+          TabItem(icon: Icons.home, title: 'Home'),
+          TabItem(icon: Icons.map, title: 'Discovery'),
+          TabItem(icon: Icons.add, title: 'Add'),
+          TabItem(icon: Icons.message, title: 'Message'),
+          TabItem(icon: Icons.people, title: 'Profile'),
+        ],
+        onTap: (int i) => print('click index=$i'),
+      )),
+    );
   }
 }
